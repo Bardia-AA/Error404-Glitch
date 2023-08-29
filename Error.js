79d7c139ee8096f9bf6f40459203769d90a52db8
Bardia-AA <bardia.asgari2015@gmail.com>
2023-08-29 08:54:51 +0330

    Add B.A

--- Error.js
+++ Error.js
@@ -1,18 +1,20 @@
-// Cache container and article elements
+﻿// written by Bardia Asgari Ahi (☞ﾟヮﾟ)☞
+// Cache container and article elements B.A
 const container = document.querySelector('.container');
 const article = document.querySelector('article');
-// Create particle container
+// Create particle container B.A
 const particleContainer = document.createElement('div');
 particleContainer.classList.add('particle-container');
-// Insert particle container before article
+// Insert particle container before article B.A
 container.insertBefore(particleContainer, article);
-// Generate particles HTML
+// Generate particles HTML B.A
 const particles = [];
 for (let i = 0; i < 20; i++) {
     particles.push('<span class="particle">4</span>');
     particles.push('<span class="particle">0</span>');
 }
 const particlesHtml = particles.join('');
-// Append particles HTML to particle container using DocumentFragment
+// Append particles HTML to particle container using DocumentFragment B.A
 const fragment = document.createRange().createContextualFragment(particlesHtml);
 particleContainer.appendChild(fragment);
+// written by Bardia Asgari Ahi (☞ﾟヮﾟ)☞
